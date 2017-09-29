01d0698dca8547d378c82debb8a6ba3c970501a1
Lena Venable <venablena@gmail.com>
2017-09-29 12:11:49 -0700

    Fill calendar with correct days in a month

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ console.log ("sanity check!")
 
 //get current time parameters
 var today = new Date()
-var year = today.getFullYear()
-var month = today.getMonth()
-var weekday = today.getDay()
-var date = today.getDate()
+const year = today.getFullYear()
+const month = today.getMonth()
+const weekday = today.getDay()
+const date = today.getDate()
 today = null //reinitialize the current date for next page load
 
 //create an array with the names of months
@@ -38,12 +38,38 @@ firstDayOfMonth = null //reinitialize the current month for next page load
 //display the current month and year in the calendar header
 document.getElementById("current-month").textContent = months[month] + year
 //create first row of calendar with weekdays: first select the correct HTML component
-var weekdayHeader = document.querySelector(".header-weekdays")
+const weekdayHeader = document.querySelector(".header-weekdays")
 //and then populate it with the weekdays array
 weekdays.forEach((day)=> {
-   var cell = document.createElement("div")
-   cell.textContent = day.slice(0, 3)//abbreviates weekdays
-   weekdayHeader.appendChild(cell)
+   let cellWeek = document.createElement("div")
+   cellWeek.className = "cell week"
+   cellWeek.textContent = day.slice(0, 3)//abbreviate weekdays
+   weekdayHeader.appendChild(cellWeek)
 })
+//create calendar rows with dates: first select the correct HTML component
+const calendar = document.querySelector(".calendar-dates")
+//create empty cells until the start of the current month
+for (let i = 0; i < firstWeekday; i++) {
+  let cellPast = document.createElement("div")
+  cellPast.className = "cell inactive"
+  calendar.appendChild(cellPast)
+}//Do I need to reinitialize i or does firstDayOfMonth=null do the trick???
 
-var calendar = document.getElementById("monthly-view")
+//create cells with a date, starting at the first weekday of the current month until the end of the month
+let day = 0
+while (day < monthDays[month]) {
+  let cellCurrent = document.createElement("div")
+  cellCurrent.className = "cell current"
+  cellCurrent.textContent = day + 1
+  calendar.appendChild(cellCurrent)
+  day += 1
+}
+
+//create empty cells to fill the last calendar row until Sat, if necessary: first find out how many items are in the calendar
+let calendarSize = calendar.children
+//if it's not divisible by 7, add empty cells until it is
+while (calendarSize.length % 7 !== 0){
+  let cellFuture = document.createElement("div")
+  cellFuture.className = "cell inactive"
+  calendar.appendChild(cellFuture)
+}
