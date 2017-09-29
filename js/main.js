aded3605b76730d203381251f5376a6b0c8a398b
Lena Venable <venablena@gmail.com>
2017-09-28 17:18:20 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log ("sanity check!")
+// get current date, month and year parameters
+var today = new Date()
+var currentYear = today.getFullYear()
+var currentMonth = today.getMonth()
+var currentWeekday = today.getDay()
+
+console.log(today);
+console.log(currentYear);
+console.log(currentMonth);
+console.log(currentWeekday);
