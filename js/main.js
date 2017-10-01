d9537d573994ac3bac1e859c0ea6aa6f29bd19c2
Lena Venable <venablena@gmail.com>
2017-09-30 20:56:56 -0700

    restructuring functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 console.log ("sanity check!")
+
 //==============================
 // Setting up the calendar
 //==============================
 
 //get current time parameters
 var today = new Date()
-const year = today.getFullYear()
+var year = today.getFullYear()
 var month = today.getMonth()
 //const month = 9
 const weekday = today.getDay()
 const date = today.getDate()
-today = null //reinitialize the current date for next page load
+today = null //reinitialize the current date for next page load (???)
 
 //create an array with the names of months
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -27,119 +28,138 @@ const monthDays = [31, (leapYear(year)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 3
 //create an array with the names of weekdays
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-//find on which weekday the month starts
-var firstDayOfMonth = new Date(year, month, 1)
-var firstWeekday = firstDayOfMonth.getDay()
-firstDayOfMonth = null //reinitialize the current month for next page load
-
 //=====================================
 // Drawing the calendar
 //=====================================
+var calendar = document.querySelector(".calendar-dates")
+var currentMonth //this becomes the selctor for the active month after it is drawn
 
-//display the current month and year in the calendar header
-document.getElementById("current-month").textContent = months[month] + year
-//create first row of calendar with weekdays: first select the correct HTML component
-const weekdayHeader = document.querySelector(".header-weekdays")
-//and then populate it with the weekdays array
-weekdays.forEach((day)=> {
-   let cellWeek = document.createElement("div")
-   cellWeek.className = "cell week"
-   cellWeek.textContent = day.slice(0, 3)//abbreviate weekdays
-   weekdayHeader.append(cellWeek)
+document.addEventListener("DOMContentLoaded", function(){
+  drawHeader()
+  drawCalendar()
+  findToday()
 })
-//create calendar rows with dates: first select the correct HTML component
-const calendar = document.querySelector(".calendar-dates")
-//create empty cells until the start of the current month
-for (let i = 0; i < firstWeekday; i++) {
-  let cellPast = document.createElement("div")
-  cellPast.className = "cell inactive"
-  calendar.append(cellPast)
-}//Do I need to reinitialize i or does firstDayOfMonth=null do the trick???
 
-//create cells with a date, starting at the first weekday of the current month until the end of the month
-let day = 1
-while (day <= monthDays[month]) {
-  let cellCurrent = document.createElement("div")
-  cellCurrent.className = "cell active"
-  cellCurrent.id = (month+1) + "-" + day + "-" + year//give each cell the id of the date they represent
-  cellCurrent.textContent = day
-  calendar.append(cellCurrent)
-  day += 1
+function drawHeader(){
+  const weekdayHeader = document.querySelector(".header-weekdays")
+  //populate the weekdays from the array
+  weekdays.forEach((day)=> {
+     let cellWeek = document.createElement("div")
+     cellWeek.className = "cell week"
+     cellWeek.textContent = day.slice(0, 3)//abbreviate weekdays
+     weekdayHeader.append(cellWeek)
+  })
 }
 
-//create empty cells to fill the last calendar row, if necessary: first find out how many items are in the calendar
-let calendarSize = calendar.children
-//and if it's not divisible by 7, pad with empty cells until it is
-while (calendarSize.length % 7 !== 0){
-  let cellFuture = document.createElement("div")
-  cellFuture.className = "cell inactive"
-  calendar.append(cellFuture)
-}
-
-//========================================
-// Populating calendar content
-//========================================
-//select the calendar cells that belong to the current month
-let currentMonth = document.querySelectorAll(".cell.active")
-var dailyView = document.querySelector("#daily-view")
-//select the hidden popup section
-let popup = document.querySelector("#popup")
-//this array is for the weekly view (later)
-var descriptionsArray = []
-
-currentMonth.forEach((calendarCell, index)=>{
-  //find today
-  let now = ((month+1) + "-" + date + "-" + year).toString()
-  if(calendarCell.id === now){
-    calendarCell.classList.add("today")
-    now = null
-  }
-  //put events in cells with an id that matches the event date
-  for (let i = 0; i < events.length; i++) {
-    if(calendarCell.id === events[i].date){
-      let event = document.createElement("p")
-      event.className = events[i].center + " " + events[i].frequency
-      event.id = events[i].id
-      event.textContent = events[i].name
-      calendarCell.append(event)
-
-      //create an element with the event's description, time and center
-      descriptionsArray.push(events[i].brief)//for the weekly view
-      let startTime = document.createElement("p")
-      startTime.textContent = events[i].timeStart
-      let endTime = document.createElement("p")
-      endTime.textContent = events[i].timeEnd
-      let title = document.createElement("h3")
-      title.textContent = events[i].name
-      let center = document.createElement("p")
-      center.textContent = events[i].center
-      dailyView.append(startTime, endTime, title, center)
-      // dailyview.append(endTime)
-      // dailyview.append()
-      //create repeat instances for weekly events:
-      // if (events[i].frequency === "weekly") {
-      //   for (let j = index + 7; j < currentMonth.length; j += 7) {
-      //     let weeklyEvent = document.createElement("p")
-      //     weeklyEvent.className = events[i].center + " " + events[i].frequency
-      //     weeklyEvent.id = events[i].id
-      //     weeklyEvent.textContent = events[i].name
-      //     currentMonth[j].append(weeklyEvent)
-      //     descriptionsArray.push(events[i].brief)
-      //   }
-      // }
+function findToday(){
+  currentMonth.forEach((calendarCell, index)=>{
+    let now = ((month+1) + "-" + date + "-" + year).toString()
+    if(calendarCell.id === now){
+      calendarCell.classList.add("today")
+      //now = null
     }
+  })
+}
+
+function drawCalendar(){
+  //display the current month and year in the calendar header
+  document.getElementById("current-month").textContent = months[month] + year
+
+  //find on which weekday the month starts
+  var firstDayOfMonth = new Date(year, month, 1)
+  var firstWeekday = firstDayOfMonth.getDay()
+  firstDayOfMonth = null //reinitialize the parameters for next page load
+  firstWeekday = null
+
+  //create empty cells until the start of the current month
+  for (let i = 0; i < firstWeekday; i++) {
+    let cellPast = document.createElement("div")
+    cellPast.className = "cell inactive"
+    calendar.append(cellPast)
   }
-  //interaction
-  calendarCell.addEventListener('mouseenter', function(){
-    dailyView.classList.remove("hidden")
-    // daily.textContent = descriptionsArray[index]
-  })
 
-  calendarCell.addEventListener('mouseleave', function() {
-    dailyView.classList.add("hidden")
-  })
-})
+  //create cells with a date, starting at the first weekday of the current month until the end of the month
+  let day = 1
+  while (day <= monthDays[month]) {
+    let cellCurrent = document.createElement("div")
+    cellCurrent.className = "cell active"
+    cellCurrent.id = (month+1) + "-" + day + "-" + year//give each cell the id of the date they represent
+    cellCurrent.textContent = day
+    calendar.append(cellCurrent)
+    day += 1
+  }
 
+  //create empty cells to fill the last calendar row, if necessary: first find out how many items are in the calendar
+  let calendarSize = calendar.children
+  //and if it's not divisible by 7, pad with empty cells until it is
+  while (calendarSize.length % 7 !== 0){
+    let cellFuture = document.createElement("div")
+    cellFuture.className = "cell inactive"
+    calendar.append(cellFuture)
+  }
+
+  //Populating calendar content :
+  //select the calendar cells that belong to the current month
+  currentMonth = document.querySelectorAll(".cell.active")
+  var dailyView = document.querySelector("#daily-view")
+  //select the hidden popup section
+  let popup = document.querySelector("#popup")
+  //this array is for the weekly view (later)
+  var descriptionsArray = []
+
+  currentMonth.forEach((calendarCell, index)=>{
+    //find today
+    // let now = ((month+1) + "-" + date + "-" + year).toString()
+    // if(calendarCell.id === now){
+    //   calendarCell.classList.add("today")
+      //now = null
+    //}
+    //put events in cells with an id that matches the event date
+    for (let i = 0; i < events.length; i++) {
+      if(calendarCell.id === events[i].date){
+        let event = document.createElement("p")
+        event.className = events[i].center + " " + events[i].frequency
+        event.id = events[i].id
+        event.textContent = events[i].name
+        calendarCell.append(event)
+
+        //create an element with the event's description, time and center
+        descriptionsArray.push(events[i].brief)//for the weekly view
+        let startTime = document.createElement("p")
+        startTime.textContent = events[i].timeStart
+        let endTime = document.createElement("p")
+        endTime.textContent = events[i].timeEnd
+        let title = document.createElement("h3")
+        title.textContent = events[i].name
+        let center = document.createElement("p")
+        center.textContent = events[i].center
+        dailyView.append(startTime, endTime, title, center)
+        // dailyview.append(endTime)
+        // dailyview.append()
+        //create repeat instances for weekly events:
+        // if (events[i].frequency === "weekly") {
+        //   for (let j = index + 7; j < currentMonth.length; j += 7) {
+        //     let weeklyEvent = document.createElement("p")
+        //     weeklyEvent.className = events[i].center + " " + events[i].frequency
+        //     weeklyEvent.id = events[i].id
+        //     weeklyEvent.textContent = events[i].name
+        //     currentMonth[j].append(weeklyEvent)
+        //     descriptionsArray.push(events[i].brief)
+        //   }
+        // }
+      }
+    }
+    //interaction
+    calendarCell.addEventListener('mouseenter', function(){
+      dailyView.classList.remove("hidden")
+      // daily.textContent = descriptionsArray[index]
+    })
+
+    calendarCell.addEventListener('mouseleave', function() {
+      dailyView.classList.add("hidden")
+    })
+  })
+};
 //===========================================
 // Interactions with the calendar
 //===========================================
@@ -168,6 +188,32 @@ currentMonth.forEach((calendarCell, index)=>{
 // Next & Previous month
 //==========================================
 document.querySelector(".fa-chevron-right").addEventListener('click', function(){
-  month = month + 1
-  console.log(month);
+  calendar.innerHTML = "" //Find a better way!?
+  if(month < 11){
+    month += 1
+  }else{
+    month = 0
+    year += 1
+  }
+  drawCalendar()
+  //remove today
+  // let cells = document.querySelectorAll(".cell.active")
+  // cells.forEach((calendarCell)=>{
+  //   calendarCell.classList.remove("today")
+  // })
+})
+
+document.querySelector(".fa-chevron-left").addEventListener('click', function(){
+  calendar.innerHTML = "" //Find a better way!?
+  if(month > 0){
+    month -= 1
+  }else{
+    month = 11
+    year -= 1
+  }
+  drawCalendar()
+  // let cells = document.querySelectorAll(".cell.active")
+  // cells.forEach((calendarCell)=>{
+  //   calendarCell.classList.remove("today")
+  // })
 })
